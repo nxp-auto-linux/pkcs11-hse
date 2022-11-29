14b00a4c5187d889c41987dcf7c1d3b5c90340bc
Vlad Pelin <vlad.pelin@nxp.com>
2022-11-29 15:09:00 +0200

    hse: add support for hse_memcpy and hse_memset
    due to restrictions imposed on hse reserved memory
    exposed through uio, it is not possible to use
    standard string operations to access this memory
    
    the solution is to add non-libc counterparts to
    these operations that take care to respect the
    alignement restrictions of this memory
    
    Issue: ALB-9559
    Signed-off-by: Vlad Pelin <vlad.pelin@nxp.com>

--- libhse/libhse.h
+++ libhse/libhse.h
@@ -13,15 +13,24 @@
 #define HSE_CHANNEL_ANY    0xACu /* use any channel, no request ordering */
 #define HSE_CHANNEL_ADM    0u /* channel reserved for administrative services */
 
+#define DECLARE_SET_ZERO(stype, sname) \
+	typeof(stype) ((sname)) = {0}
+
 int hse_dev_open(void);
 void hse_dev_close(void);
 
 uint16_t hse_check_status(void);
 
-int hse_srv_req_sync(uint8_t channel, const void *srv_desc);
+int hse_srv_req_sync(uint8_t channel, const void *srv_desc, size_t size);
 
 void *hse_mem_alloc(size_t size);
 void hse_mem_free(void *addr);
+
+/* mapped hse-accesible memory has certain restrictions that makes the use of
+ * standard string routines impossible; these should be used instead of their
+ * libc counterparts */
+void *hse_memcpy(void *dest, const void *src, size_t size);
+void *hse_memset(void *dest, int fill, size_t size);
 
 uint64_t hse_virt_to_dma(const void *addr);
 
